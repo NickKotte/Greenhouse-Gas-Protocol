92167be12b57bcf55d872c018150c47dac37c2cb
Nick Kotte <nickkotte@gmail.com>
2024-07-26 11:35:14 -0400

    vercel bug fix and default color scheme dark

--- src/main.tsx
+++ src/main.tsx
@@ -19,7 +19,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<ColorSchemeScript defaultColorScheme="auto" />
-		<MantineProvider theme={mantineTheme} defaultColorScheme="auto">
+		<MantineProvider theme={mantineTheme} defaultColorScheme="dark">
 			<QueryClientProvider client={queryClient}>
 				<Notifications />
 				<CustomSpotlight />
